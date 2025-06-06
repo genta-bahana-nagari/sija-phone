dd227f96b7c436f9de583e1d1dea8d300e5c6664
genta-bahana-nagari <gentapossenti@gmail.com>
2025-06-06 19:53:40 +0700

    revisi route

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PhoneController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [HomeController::class, 'index']);
+Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/load-more', [HomeController::class, 'loadMore']);
-Route::get('/phones/see-all', [PhoneController::class, 'seeAll'])->name('phones.see-all');
+Route::get('phones/see-all', [PhoneController::class, 'seeAll'])->name('phones.see-all');
+
