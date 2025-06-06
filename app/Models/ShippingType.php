ecb6ec087b287b41aeb0e7fa07c00fde7d50de9f
genta-bahana-nagari <gentapossenti@gmail.com>
2025-06-06 10:25:02 +0700

    shipping type

---
+++ app/Models/ShippingType.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ShippingType extends Model
+{
+    use HasFactory;
+
+    protected $table = 'shipping_types';
+
+    protected $fillable = ['tipe_pengiriman'];
+
+    public function orders()
+    {
+        return $this->hasMany(Order::class, 'shipping_type_id');
+    }
+}
