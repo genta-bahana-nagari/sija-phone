97a3234e8bd61fb376c117cb75600cc054aadeba
genta-bahana-nagari <gentapossenti@gmail.com>
2025-06-06 22:02:08 +0700

    route checkout v1

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,27 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PhoneController;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CheckoutController;
 
-Route::get('/about-store', function () {
-    return view('about-store');
-})->name('about.store');
-
+// Beranda
 Route::get('/', [HomeController::class, 'index'])->name('home');
-Route::get('/load-more', [HomeController::class, 'loadMore']);
+Route::get('/load-more', [HomeController::class, 'loadMore'])->name('home.loadMore');
 
-// Untuk Smartphones
-Route::get('phones/see-all', [PhoneController::class, 'seeAll'])->name('phones.see-all');
-Route::get('/phones/{id}', [PhoneController::class, 'show'])->name('phones.show');
+// Informasi Toko
+Route::view('/about-store', 'about-store')->name('about.store');
+
+// Produk (Smartphones)
+Route::prefix('phones')->group(function () {
+    Route::get('/see-all', [PhoneController::class, 'seeAll'])->name('phones.see-all');
+    Route::get('/{id}', [PhoneController::class, 'show'])->name('phones.show');
+});
+
+// Checkout
+// web.php
+Route::prefix('checkout')->group(function () {
+    Route::get('/', [CheckoutController::class, 'index'])->name('checkout');
+    Route::post('/', [CheckoutController::class, 'store'])->name('checkout.store');
+    Route::post('/from-product', [CheckoutController::class, 'checkoutFromProduct'])->name('checkout.fromProduct');
+});
