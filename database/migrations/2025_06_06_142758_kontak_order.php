e30eb3291001d990dc223408b12e510e811c82d0
genta-bahana-nagari <gentapossenti@gmail.com>
2025-06-06 21:36:58 +0700

    ada yang salah

--- database/migrations/2025_06_06_142758_kontak_order.php
+++ database/migrations/2025_06_06_142758_kontak_order.php
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('orders', function($table) {
-            $table->dropColumn('shipping_type_id');
+            $table->dropColumn('kontak');
         });
     }
 };
