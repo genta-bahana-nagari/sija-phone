06c95a9242ea503240b8b39daa19a59b64c8f2dd
genta-bahana-nagari <gentapossenti@gmail.com>
2025-06-07 10:21:30 +0700

    gambar biar sama

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -66,7 +66,7 @@
             <div class="grid grid-cols-2 lg:grid-cols-5 md:grid-cols-4 gap-4" id="product-list">
                 @foreach ($phonesForBrands as $phone)
                 <div onclick="window.location='{{ route('phones.show', $phone->id) }}'" class="border p-4 rounded-lg shadow-sm hover:shadow-md transition cursor-pointer">
-                    <img src="{{ asset('storage/' . $phone->gambar) }}" alt="{{ $phone->tipe }}" class="object-cover">
+                    <img src="{{ asset('storage/' . $phone->gambar) }}" alt="{{ $phone->tipe }}" class="object-cover w-full h-48 mb-3">
                     <div class="p-3">
                         <p class="text-xs text-gray-400 mb-1">{{ $phone->tipe }}</p>
                         <h3 class="text-sm font-semibold leading-tight mb-1">{{ Str::limit($phone->deskripsi, 50) }}</h3>
