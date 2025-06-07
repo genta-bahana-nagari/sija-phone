f3a3c82056e5f24df0991f2473fd923c8e265499
genta-bahana-nagari <gentapossenti@gmail.com>
2025-06-07 11:42:56 +0700

    revisi kecil

--- resources/views/layouts/welcome.blade.php
+++ resources/views/layouts/welcome.blade.php
@@ -43,7 +43,7 @@
                         <a href="#" class="hover:text-orange-500">Promo</a>
                         <a href="#" class="hover:text-orange-500">Terlaris</a>
                         <a href="#" class="hover:text-orange-500">Terbaru</a>
-                        <a href="{{ route('order.history') }}" class="hover:text-orange-500">Daftar Pesanan</a>
+                        <a href="{{ route('orders.history') }}" class="hover:text-orange-500">Daftar Pesanan</a>
                     </nav>
                 </div>
             </div>
@@ -51,7 +51,7 @@
             <!-- Kanan: Icons -->
             <div class="flex items-center gap-x-6">
                 <!-- Keranjang -->
-                <a href="#" title="Keranjang">
+                <a href="{{ route('cart.index') }}" title="Keranjang">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
                     </svg>
@@ -111,7 +111,7 @@
                                 </svg>
                                 <span>Profil Saya</span>
                             </a>
-                            <a href="{{ route('order.history') }}"
+                            <a href="{{ route('orders.history') }}"
                                 class="flex items-center space-x-2 px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z" />
@@ -203,9 +203,9 @@
         <div class="max-w-7xl mx-auto px-4 py-10 grid grid-cols-2 md:grid-cols-4 gap-8">
             <!-- Informasi Phoneshop -->
             <div>
-            <h4 class="font-semibold mb-3">SIJA Phone</h4>
+            <h4 lass="font-semibold mb-3">SIJA Phone</h4>
             <ul class="space-y-2">
-                <li><a href="#">Tentang SIJA Phone</a></li>
+                <li><a href="{{ route('about.store') }}">Tentang SIJA Phone</a></li>
                 <li><a href="#">Lorem Ipsum</a></li>
                 <li><a href="#">Karir</a></li>
                 <li><a href="#">Promo Hari Ini</a></li>
